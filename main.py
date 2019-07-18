197256ea2ba117ae205837308d717e65e279ec9f
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-18 10:35:11 -0400

    adding main.py,new_post.html, view_post.html

--- main.py
+++ main.py
@@ -12,7 +12,26 @@ class MainHandler(webapp2.RequestHandler):
         template = jinja_env.get_template('index.html')
         self.response.write(template.render())
 
-app = webapp2.WSGIApplication([
-        ('/', MainHandler)
+class Bloghandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_env.get_template('new_post.html')
+        self.response.write(template.render())
+    def post(self):
+        Title_answer: self.request.get('Title')
+        Name_answer: self.request.get('Name')
+        Content_answer: self.request.get('Content')
 
+        template_vars = {
+        "Title_answer" : Title_answer,
+        "Name_answer" : Name_answer,
+        "Content_answer" : Content_answer
+        }
+        template = jinja_env.get_template('templates/view_post.html')
+        self.response.write(template.render(template_vars))
+
+
+app = webapp2.WSGIApplication([
+        ('/', MainHandler),
+        ('/blogpost', Bloghandler),
+        ('/view', Bloghandler)
 ] ,debug=True)
